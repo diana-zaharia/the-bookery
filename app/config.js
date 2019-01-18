346a40b65ac060a9ee9cec93f4ad6ad0d8cdd5a3
Diana Zaharia <dianaszaharia@gmail.com>
2019-01-18 11:35:08 +0200

    updated mongodb connection url

--- app/config.js
+++ app/config.js
@@ -1,4 +1,3 @@
 module.exports = {
-	'secret': 'nobananastoday',
-	'database': 'mongodb://localhost/fii'
+	'database': 'mongodb://heroku_tsc8c786:dqqeuuslehv6au5ua73ljr0cep@ds161024.mlab.com:61024/heroku_tsc8c786'
 };
